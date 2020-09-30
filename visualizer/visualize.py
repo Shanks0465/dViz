146f87ae38b9ebc7bbf082f591b9264da39ab477
Shanks0465 <umashanks99@gmail.com>
2020-09-30 13:44:53 +0530

    Added visualizer

---
+++ visualizer/visualize.py
@@ -0,0 +1,48 @@
+from graphviz import Digraph
+import keras;
+from keras.models import Sequential;
+from keras.layers import Dense
+import random
+
+def viewSimpleNN(model,name,colored=False,cluster=False,labelled=False):
+  """ Return Architecture Diagram of given Simple Deep Neural Network"""
+  dense=[]
+  units=[]
+  layers=[]
+  dense.append("input")
+  units.append(model.layers[0].input_shape[1])
+  m = Digraph(name,format='png')
+  m.attr('node',shape='circle')
+  if cluster==True:
+    layer_name="cluster_"
+  else:
+    layer_name="layer_"
+  for layer in model.layers:
+    if(type(layer)==keras.layers.core.Dense):
+      dense.append(layer.name)
+      units.append(layer.output_shape[1])
+  for i in range(len(dense)):
+    with m.subgraph(name=layer_name+str(i)) as c:
+      if cluster==True:
+        c.attr(label="layer_"+str(i),labeljust="l",height="5",width="10",rank='same')
+      if colored==True:
+        col_value="#"+"%06x" % random.randint(0, 0xFFFFFF)
+        c.node_attr.update(style='filled', color=col_value, fontcolor=col_value)
+      else:
+        c.node_attr.update(fontcolor="white")
+      layer=dense[i]
+      unit=units[i]
+      cache=[]
+      for j in range(unit):
+        c.node(layer+str(j))
+        cache.append(layer+str(j))
+      layers.append(cache)
+  i=0;
+  while i<len(layers)-1:
+    j=i+1
+    for x in layers[i]:
+      for y in layers[j]:
+        m.edge(x,y)
+    i+=1
+  m.render()
+  return m
